932889a597f1de8a29c2b0dcc565e5fff6c7e31a
Matias Yanez <matias.yanez@usach.cl>
2022-11-18 21:56:30 -0300

    implementacion vista justificativo

---
+++ my-mueblesstgo/src/services/JustificativoService.js
@@ -0,0 +1,12 @@
+import axios from 'axios';
+
+const JUSTIFICATIVO_API_URL = "http://localhost:8080/justificativo";
+
+class JustificativoService{
+
+    createJustificativo(justificativo){
+        return axios.post(JUSTIFICATIVO_API_URL + "/subirJustificativo", justificativo);
+    }
+}
+
+export default new JustificativoService()
